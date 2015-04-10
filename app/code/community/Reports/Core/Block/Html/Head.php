aa1570ca50c3757d2a513377e37827d5fe4ddd22
LorenzoR <lorenzorodrigo2000@hotmail.com>
2015-04-10 15:28:31 -0400

    change module name

--- app/code/community/Reports/Core/Block/Html/Head.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Reports_Core_Block_Html_Head extends Mage_Page_Block_Html_Head {
-     
-     protected function _construct() {
-          $this->setTemplate('page/html/head.phtml');
-     }
-}
-
-?>
